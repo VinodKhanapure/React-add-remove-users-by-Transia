8ffb3d9a24b1eddc00c3f93d8e89baaec342144c
VinodKhanapure <vinuk8235@gmail.com>
2020-01-31 12:33:13 +0530

    Updates

---
+++ precache-manifest.fadc9b9d6e9e2c7b3be2667e2f7056ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d2c9938bc1f8a65b41beb07f620373",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1b3d69870bc608685d9",
+    "url": "/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "28f88d6e7bf2a5d74419",
+    "url": "/static/js/2.d35a2c42.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d35a2c42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f1b3d69870bc608685d9",
+    "url": "/static/js/main.53127894.chunk.js"
+  },
+  {
+    "revision": "ed6c4f129452b2edba17",
+    "url": "/static/js/runtime-main.9a8399c6.js"
+  }
+]);
